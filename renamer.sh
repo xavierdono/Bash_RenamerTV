2e96fcd8efcdb7fa5a5a9f5facafc3fdd6799ed0
Xavier DONO <xavierdono@users.noreply.github.com>
2017-07-28 22:13:32 +0200

    Ajout de contrôle si la série/épisode n'existe pas

--- renamer.sh
+++ renamer.sh
@@ -13,18 +13,26 @@ showext=${fname##*.}
 
 show_id=$(curl -s $urlshow${showname/./ } | jq -r '.id')
 
-if [[ "$filename" =~(\**).*(.*[0-9][0-9]).*([0-9][0-9]).*(\....)$ ]]; then
-    show_season=${BASH_REMATCH[2]}
-    show_episode=${BASH_REMATCH[3]}
+if [ "$show_id" == "" ]; then
+	zenity --info --text="Série non trouvée"
+else
+    if [[ "$filename" =~(\**).*(.*[0-9][0-9]).*([0-9][0-9]).*(\....)$ ]]; then
+        show_season=${BASH_REMATCH[2]}
+        show_episode=${BASH_REMATCH[3]}
+    fi
+
+    urlFullName="https://api.tvmaze.com/shows/$show_id/episodebynumber?season=$show_season&number=$show_episode"
+
+    name=$(curl -s $urlFullName | jq -r '.name')
+
+    if [ "$name" == "Not Found" ]; then
+	    zenity --info --text="Episode non trouvé"
+    else
+        show=${showname/./ }
+        finalshow=(${show,,})
+
+        realnameshow="${finalshow[@]^} - ${show_season}x${show_episode} - ${name/:/ }.$showext"
+
+        mv "$filename" "$dirname/$realnameshow"
+    fi
 fi
-
-urlFullName="https://api.tvmaze.com/shows/$show_id/episodebynumber?season=$show_season&number=$show_episode"
-
-name=$(curl -s $urlFullName | jq -r '.name')
-
-show=${showname/./ }
-finalshow=(${show,,})
-
-realnameshow="${finalshow[@]^} - ${show_season}x${show_episode} - ${name/:/ }.$showext"
-
-mv "$filename" "$dirname/$realnameshow"
